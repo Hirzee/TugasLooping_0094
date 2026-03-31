274b9ae2f6c2a423f0de0fb502ee94310221d9be
Hirzee <faruqhirzi698@gmail.com>
2026-03-31 09:09:00 +0700

    prima

--- looping.cpp
+++ looping.cpp
@@ -7,3 +7,12 @@ int pilihan;
 int isPrima(int n) {
     if (n <= 1) return 0;
 
+    int i = 2;
+    while (i <= n / 2) {
+        if (n % i == 0) {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
